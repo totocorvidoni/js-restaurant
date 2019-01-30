db882ae24590d6b88d0fba8e58954bd98078e141
Toto Corvidoni <totocorvidoni@gmail.com>
2019-01-30 17:11:28 -0300

    Refactor JS functions.

--- main.js
+++ main.js
@@ -94,7 +94,19 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_page_load__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/page-load */ \"./src/modules/page-load.js\");\n\n\nObject(_modules_page_load__WEBPACK_IMPORTED_MODULE_0__[\"pageLoad\"])();\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_page_load__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/page-load */ \"./src/modules/page-load.js\");\n/* harmony import */ var _modules_about__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/about */ \"./src/modules/about.js\");\n\n\n\nObject(_modules_page_load__WEBPACK_IMPORTED_MODULE_0__[\"pageLoad\"])();\nObject(_modules_about__WEBPACK_IMPORTED_MODULE_1__[\"renderAbout\"])();\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/modules/about.js":
+/*!******************************!*\
+  !*** ./src/modules/about.js ***!
+  \******************************/
+/*! exports provided: renderAbout */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"renderAbout\", function() { return renderAbout; });\nconst renderAbout = () => {\n  const content = document.querySelector('#content');\n  let main = document.createElement('main');\n  let heading = document.createElement('h2');\n  heading.appendChild(document.createTextNode('Pretium Sagittis'));\n  let p = document.createElement('p');\n  let text = document.createTextNode('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique senectus et netus et malesuada fames ac turpis. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Non tellus orci ac auctor augue. Sit amet dictum sit amet justo donec enim diam. Morbi tincidunt augue interdum velit.');\n  p.appendChild(text);\n  main.appendChild(heading);\n  main.appendChild(p);\n  content.appendChild(main);\n}\n\n\n\n//# sourceURL=webpack:///./src/modules/about.js?");
 
 /***/ }),
 
@@ -102,11 +114,11 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mod
 /*!**********************************!*\
   !*** ./src/modules/page-load.js ***!
   \**********************************/
-/*! exports provided: pageLoad, about */
+/*! exports provided: pageLoad */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"pageLoad\", function() { return pageLoad; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"about\", function() { return about; });\nconst pageLoad = () => {\n  const header = document.querySelector('header');\n  header.innerHTML = `\n    <div class=\"logo\">\n      <i class=\"fas fa-mountain fa-5x\"></i>\n      <h1 >Valhalla</h1>\n    </div>\n    <p>If you are going to eat, mind as well do it with the gods.</p>\n    `;\n  about();\n}\n\nconst about = () => {\n  const content = document.querySelector('#content');\n  content.innerHTML = `\n    <h2>Pretium Sagittis</h2>\n    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \n       tempor incididunt ut labore et dolore magna aliqua. Tristique senectus \n       et netus et malesuada fames ac turpis. Tristique sollicitudin nibh sit \n       amet commodo nulla facilisi nullam vehicula. Non tellus orci ac auctor \n       augue. Sit amet dictum sit amet justo donec enim diam. Morbi tincidunt \n       augue interdum velit.</p>\n  `\n}\n\n\n\n//# sourceURL=webpack:///./src/modules/page-load.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"pageLoad\", function() { return pageLoad; });\nconst pageLoad = () => {\n  const content = document.querySelector('#content');\n  // render header\n  let header = document.createElement('header');\n  let logoContainer = document.createElement('div');\n  logoContainer.id = 'logo';\n  let logo = document.createElement('img');\n  logo.setAttribute('src', 'images/w-logo.svg')\n  logoContainer.appendChild(logo);\n  let name = document.createElement('h1');\n  name.appendChild(document.createTextNode('Valhalla'));\n  logoContainer.appendChild(name);\n  let subText = document.createElement('p');\n  subText.appendChild(document.createTextNode('If you are going to eat, mind as well do it with the gods.'));\n  header.appendChild(logoContainer);\n  header.appendChild(subText);\n  content.appendChild(header);\n\n  // render nav\n  let nav = document.createElement('nav');\n  let about = document.createElement('a');\n  about.setAttribute('href', '#');\n  about.id = 'about';\n  about.appendChild(document.createTextNode('ABOUT US'));\n  let menu = document.createElement('a');\n  menu.setAttribute('href', '#');\n  menu.id = 'menu';\n  menu.appendChild(document.createTextNode('MENU'));\n  let contact = document.createElement('a');\n  contact.setAttribute('href', '#');\n  contact.id = 'contact';\n  contact.appendChild(document.createTextNode('CONTACT'));\n  nav.appendChild(about);\n  nav.appendChild(menu);\n  nav.appendChild(contact);\n  content.appendChild(nav);\n  // about();\n}\n\n\n\n//# sourceURL=webpack:///./src/modules/page-load.js?");
 
 /***/ })
 
